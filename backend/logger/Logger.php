2e5a432a74d4371bbbaf439f3626038f5efca5cc
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-05-13 19:21:21 +0200

    create Logger class and logs directory

---
+++ backend/logger/Logger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace logger;
+
+class Logger
+{
+    private static $logFile = '../logs/logfile.log';
+
+    public static function log($level, $message)
+    {
+        $formattedMessage = date('[Y-m-d H:i:s]') . "[$level] $message" . PHP_EOL;
+        error_log($formattedMessage, 3, self::$logFile);
+    }
+
+    public static function info($message)
+    {
+        self::log('INFO', $message);
+    }
+
+    public static function error($message) {
+        self::log('ERROR', $message);
+    }
+}

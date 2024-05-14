036ac2de4e44ec10de73505b4b77d5789d9faa27
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-05-14 14:03:38 +0200

    implement registration script

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Get forms from document
     const loginForm = document.getElementById('login-form');
     const registerForm = document.getElementById('register-form');
 
+    // Add submit event listener to login form
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
         const username = document.getElementById('login-username').value;
@@ -20,12 +22,39 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Redirect to article list page upon successful login
                     window.location.href = '/articles.html';
                 } else {
-                    // Handle login error, display error message to the user, etc.
+                    // Handle login error
                     console.error('Login failed:', response.statusText);
                 }
             })
             .catch(error => {
                 console.error('Error during login:', error);
+            });
+    });
+
+    registerForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = document.getElementById('register-username').value;
+        const password = document.getElementById('register-password').value;
+
+        // Make a request to backend to handle registration
+        fetch('/backend/api/user/register.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: username, password: password })
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Redirect to the login page or perform any other actions upon successful registration
+                    window.location.href = '/index.html';
+                } else {
+                    // Handle registration error
+                    console.error('Registration failed:', response.statusText);
+                }
+            })
+            .catch(error => {
+                console.error('Error during registration:', error);
             });
     });
 });
